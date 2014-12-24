66a4e8ff90c195b7ce98d8e5720021d8e3cfaa21
queuebit <queuebit10648@gmail.com>
2014-12-24 10:01:06 -0500

    The game _works_.

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This is a simple test api-like app I am making after doing the [Code School Buil
 * `/` --> Hello World
 * `/roshambo` --> Rock, Paper, or Scissors please 
 * `/roshambo/penny` --> You threw a penny
+
+### Ideas for things to do
+* Serve a static page where you can play in a client
+* Add some logging using [morgan](https://github.com/expressjs/morgan)
+* Add _secret_ weapons
--- app.js
+++ app.js
@@ -21,8 +21,54 @@ app.param('weapon', function(request,response,next) {
 });
 
 app.get('/roshambo/:weapon', function(request,response,next) {
-  response.json('You threw a ' + request.params.weapon);
+  var humanThrew = request.params.weapon;
+  var botThrew = botThrows();
+  var winner = whoWon(humanThrew,botThrew); 
+  var game = {
+    'human': humanThrew,
+    'bot': botThrew,
+    'winner': winner
+  };
+  response.json(game);
 });
+
+function botThrows() {
+  return weapons[Math.floor(Math.random() * 3)];
+}
+
+function whoWon(human,bot) {
+  if (human === 'rock') {
+    if (bot === 'paper') {
+      return 'bot';
+    } else if (bot === 'scissors') {
+      return 'human';
+    } else if (bot === 'rock') {
+      return 'tie'
+    } else {
+      return 'Uh Oh'
+    }
+  } else if (human === 'paper') {
+    if (bot === 'paper') {
+      return 'tie';
+    } else if (bot === 'scissors') {
+      return 'bot';
+    } else if (bot === 'rock') {
+      return 'human'
+    } else {
+      return 'Uh Oh'
+    }
+  } else if (human === 'scissors') {
+    if (bot === 'paper') {
+      return 'human';
+    } else if (bot === 'scissors') {
+      return 'tie';
+    } else if (bot === 'rock') {
+      return 'bot'
+    } else {
+      return 'Uh Oh'
+    }
+  }
+}
 
 app.listen(3001, function() {
   console.log('Rock, Paper, Scissoring on port 3001');
