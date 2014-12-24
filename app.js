2ede6e338749c7849d8d8015ab2b98c47575e9c8
queuebit <queuebit10648@gmail.com>
2014-12-24 09:39:53 -0500

    Sanitizes weapons for roshambo using middleware

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
 
+var weapons = ['rock','paper','scissors'];
+
 app.get('/', function(request,response) {
   response.send('Hello world');
 });
 
 app.get('/roshambo', function(request,response,next) {
   response.json('Rock, Paper, or Scissors please');
+});
+
+app.param('weapon', function(request,response,next) {
+  var weapon = request.params.weapon.toLowerCase();
+  if (weapons.indexOf(weapon) >= 0) {
+    next();
+  } else {
+    response.status(400).json("We are playing Roshambo here, hipster!");
+  }
 });
 
 app.get('/roshambo/:weapon', function(request,response,next) {
