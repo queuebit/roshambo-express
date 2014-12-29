1d74e8f0e56e5692ed94031d734ce59e645abac3
queuebit <queuebit10648@gmail.com>
2014-12-29 08:04:50 -0500

    Adds mocha support for tests

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 var request = require('supertest');
 var app = require('./app');
 
-request(app)
-  .get('/')
-  .expect(200)
-  .end(function(error) {
-    if(error) throw error;
-    console.log('Done');
+describe('Requests to the roshambo path', function() {
+  it('Returns a 200 status code', function(done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(error) {
+        if(error) throw error;
+        done();
+      });
   });
+});
