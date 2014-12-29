385e17b6ee70be73b34402e18320dc11ae7408ed
queuebit <queuebit10648@gmail.com>
2014-12-29 08:39:14 -0500

    Adds autorunner for mocha / npm test

--- test.js
+++ test.js
@@ -4,11 +4,25 @@ var app = require('./app');
 describe('Requests to the roshambo path', function() {
   it('Returns a 200 status code', function(done) {
     request(app)
-      .get('/')
+      .get('/roshambo')
       .expect(200)
       .end(function(error) {
         if(error) throw error;
         done();
       });
+  });
+  it('Returns a 400 status code for bad weapon', function(done) {
+    request(app)
+      .get('/roshambo/penny')
+      .expect(400)
+      .end(function(error) {
+        if(error) throw error;
+        done();
+      });
+  });
+  it('Returns a 200 status code for good weapon', function(done) {
+    request(app)
+      .get('/roshambo/rock')
+      .expect(200, done);
   });
 });
